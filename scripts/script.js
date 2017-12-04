a8cba80903d5c44e064a7986681694ef735505ee
Napster653 <31384686+PabloNapster@users.noreply.github.com>
2017-12-04 17:53:59 +0100

    Added header and dropdown menu

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+function showDropdown()
+{
+	document.getElementById('more_dropdown').classList.toggle('show');
+}
+window.onclick = function(event)
+{
+	if (!$(event.target).parents('.dropdown').length)
+	{
+		console.log("outside");
+		var dropdowns = document.getElementsByClassName('dropdown-content');
+		for (var i = 0; i < dropdowns.length; i++)
+		{
+			if (dropdowns[i].classList.contains('show'))
+				dropdowns[i].classList.remove('show');
+		}
+	}
+	else
+	{
+		console.log('inside');
+	}
+}
